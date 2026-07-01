6d06fe589f01f9d7d75ffebb1c07e557f084178d
lanbpfv-tech <lanbpfv@gmail.com>
2026-07-01 07:14:08 +0300

    ajout fonction annuler dans VoyageService.java

--- transport/src/main/java/com/cooperative/transport/services/VoyageService.java
+++ transport/src/main/java/com/cooperative/transport/services/VoyageService.java
@@ -1,10 +1,11 @@
 package com.cooperative.transport.services;
 
-import com.cooperative.transport.entities.Voyages;
-import com.cooperative.transport.repositories.VoyageRepository;
+import com.cooperative.transport.entities.*;
+import com.cooperative.transport.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VoyageService {
@@ -12,7 +13,33 @@ public class VoyageService {
     @Autowired
     private VoyageRepository voyageRepository;
 
+    @Autowired
+    private StatutVoyageRepository statutVoyageRepo;
+
+    @Autowired
+    private VoyageStatutRepository voyageStatutRepo;
+
     public List<Voyages> findAllVoyages() {
         return voyageRepository.findAllCatalogueVoyage();
+    }
+
+    // @Transactional
+    // public boolean deleteVoyage(Integer id) {
+    // if (voyageRepository.existsById(id)) {
+    // voyageRepository.deleteById(id);
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
+
+    public void annuler(Voyages voyage) {
+        VoyageStatut voyagestatut = new VoyageStatut();
+        voyagestatut.setVoyage(voyage);
+        Optional<StatutVoyage> statut = statutVoyageRepo.findByLibelle("Annulé");
+        StatutVoyage statutAnnule = statut.get();
+        voyagestatut.setStatut(statutAnnule);
+
+        voyageStatutRepo.save(voyagestatut);
     }
 }
